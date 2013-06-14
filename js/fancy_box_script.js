02b5df797a4ef6c2708e2242c533b2efb72d2ea2
unknown <madhur@madhur-PC.(none)>
2013-06-15 01:29:15 +0530

    added scripts

--- js/fancy_box_script.js
+++ js/fancy_box_script.js
@@ -1,16 +1,3 @@
-
-
-
-
-
-/*
-     FILE ARCHIVED ON 11:40:44 Mar 7, 2012 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 16:07:06 Jun 14, 2013.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
 (function($){
 	$(document).ready(function(){
 		$('a[rel="showVideo"]').click(function() {
---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+		$("#makeMeScrollable").smoothDivScroll({
+			mousewheelScrolling: "allDirections",
+			manualContinuousScrolling: true,
+			autoScrollingMode: "onStart"
+		});
+	});
