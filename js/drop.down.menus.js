82e44a9234a9bd1cbcc8e1995ac7e37006440f34
unknown <madhur@madhur-PC.(none)>
2013-06-14 23:21:34 +0530

    initial commit for devifashion

---
+++ js/drop.down.menus.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 10:55:41 Mar 7, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:07:10 Jun 14, 2013.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+(function($){
+	$.fn.dropDownMenus=function(w){
+		w=parseInt(w);
+		$(this).hover(
+			function(){
+				$(this).addClass('on');
+				elementOffset = $(this).offset();
+				elementLeft=parseInt(elementOffset.left);
+				screenWidth=$(document).width();
+				
+				if((elementLeft+w*2)>screenWidth){
+					$('ul:first',this).css('right',0)
+					$('ul ul',this).css({'left':-w,'top':0})
+				}
+				
+				else {
+					
+					$('ul ul',this).css({'left':w+'px','top':0})
+					
+				}
+				
+				$('ul',this).css('width',w)
+				$('ul:first', this).fadeIn('slow')
+				$('ul:first a', this).hover(
+					function(){$(this).addClass('on')},
+					function(){$(this).removeClass('on')}
+				)
+				
+			},
+			
+			function(){
+				$(this).removeClass('on');
+				$('ul:first', this).hide()
+				$(this).removeClass('over');
+			}
+		)
+	}
+})(jQuery)
---
+++ js/fancy_box_script.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 11:40:44 Mar 7, 2012 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 16:07:06 Jun 14, 2013.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+(function($){
+	$(document).ready(function(){
+		$('a[rel="showVideo"]').click(function() {
+			$.fancybox({
+				'autoScale'		: false,
+				'transitionIn'	: 'none',
+				'transitionOut'	: 'none',
+				'title'			: this.title,
+				'width'			: 450,
+				'height'		: 350,
+				'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+				'type'			: 'swf',
+				'swf'			: {
+				'wmode'			: 'transparent',
+				'allowfullscreen'	: 'true'
+				}
+			});
+		
+			return false;
+		});
+		
+		
+		$("a[rel='showZoomImage']").fancybox({
+			'transitionIn'	: 'elastic',
+			'transitionOut'	: 'elastic',
+			'titlePosition'	: 'inside'
+		});
+		
+		$(".dg1").fancybox({
+	     
+			'autoScale'     	: true,
+			'transitionIn'		: 'elastic',
+			'transitionOut'		: 'elastic'
+		});
+	})
+})(jQuery);
